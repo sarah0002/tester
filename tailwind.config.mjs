3c3448e516f778d62540b25c8c771ef2c495e6a5
Sarah Albaek <saal0002@stud.kea.dk>
2025-05-02 10:42:59 +0200

    o

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,160 +1,158 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
- 
-   theme: {
-     extend: {
-       colors: {
-         transparent: "transparent",
-         current: "currentColor",
-         rød: {
-           50: "#F8F2F2",
-           100: "#F1E5E6",
-           200: "#E2CACC",
-           300: "#D4B0B3",
-           400: "#C49296",
-           500: "#B6787D",
-           600: "#975357",
-           700: "#774045",
-           800: "#4F2B2E",
-           900: "#281517",
-           950: "#140B0C",
-         },
- 
-         blå: {
-           50: "#EEF0F8",
-           100: "#DEE5F2",
-           200: "#BDCBE5",
-           300: "#9EB0D8",
-           400: "#7C96CB",
-           500: "#5A7BBD",
-           600: "#4060A0",
-           700: "#304878",
-           800: "#1F1050",
-           900: "#101828",
-           950: "#070B12",
-         },
- 
-         grøn: {
-           50: "#F0F3F1",
-           100: "#E3E8E4",
-           200: "#C7D0C8",
-           300: "#ABB9AD",
-           400: "#8E9F8F",
-           500: "#738874",
-           600: "#5B6B5D",
-           700: "#465347",
-           800: "#303730",
-           900: "#171C18",
-           950: "#0C0E0C",
-         },
- 
-         brun: {
-           50: "#FCFAF7",
-           100: "#FAF5F1",
-           200: "#F5EBE0",
-           300: "#F0E0D1",
-           400: "#EBD7C2",
-           500: "#E5CBB0",
-           600: "#D2A575",
-           700: "#B87C3D",
-           800: "#7B5327",
-           900: "#3D2914",
-           950: "#1F150B",
-         },
- 
-         grå: {
-           50: "#FDFDFC",
-           100: "#F9F8F6",
-           200: "#F4F3F0",
-           300: "#EEECE6",
-           400: "#E9E7E1",
-           500: "#E4E0D9",
-           600: "#BFB6A6",
-           700: "#988B70",
-           800: "#685D4A",
-           900: "#332D24",
-           950: "#1A1814",
-         },
- 
-         hvid: '#ffffff',
-       },
- 
-       spacing: {
-         xxs: "8px",
-         xs: "15px",
-         s: "30px",
-         md: "60px",
-         lg: "80px",
-         xl: "110px",
-         "2xl": "140px",
-         "3xl": "180px",
-         "4xl": "200px",
-         "5xl": "300px",
-       },
- 
-       fontFamily: {
-         montserrat: ['"Montserrat"', "sans-serif"],
-         khand: ['"Khand"', "sans-serif"],
-       },
- 
-       fontWeight: {
-         thin: 100,
-         extraLight: 200,
-         light: 300,
-         regular: 400,
-         medium: 500,
-         semiBold: 600,
-         bold: 700,
-         extraBold: 800,
-         black: 900,
-       },
- 
-       fontSize: {
-         p_desk: ["18px", { lineHeight: "1.75rem" }],
-         p_stordesk: ["20px", { lineHeight: "40px" }],
-         p_lilledesk: ["16px", { lineHeight: "1.75rem" }],
-         h4_desk: ["24px", { lineHeight: "2.25rem" }],
-         h3_desk: ["31px", { lineHeight: "3rem" }],
-         h2_desk: ["58px", { lineHeight: "3.75rem" }],
-         h1_desk: ["96px", { lineHeight: "4.5rem" }],
- 
-         p_stormobil: ["14px", { lineHeight: "1.50rem" }],
-         p_mobil: ["12px", { lineHeight: "1.50rem" }],
-         p_lillemobil: ["10px", { lineHeight: "1.50rem" }],
-         h4_mobil: ["18px", { lineHeight: "2.25rem" }],
-         h3_mobil: ["24px", { lineHeight: "3rem" }],
-         h2_mobil: ["32px", { lineHeight: "" }],
-         h1_mobil: ["48px", { lineHeight: "4.5rem" }],
-       },
- 
-       // Animationer og keyframes
-       animation: {
-         rotate: 'rotate 3s linear infinite',
-         scaleUp: 'scaleUp 3s infinite',
-         moveRight: 'moveRight 3s infinite',
-       },
-       keyframes: {
-         rotate: {
-           '0%': { transform: 'rotate(0deg)' },
-           '100%': { transform: 'rotate(360deg)' },
-         },
-         scaleUp: {
-           '0%': { transform: 'scale(1)' },
-           '100%': { transform: 'scale(1.5)' },
-         },
-         moveRight: {
-           '0%': { transform: 'translateX(0)' },
-           '100%': { transform: 'translateX(50px)' },
-         },
-       },
-     },
-   },
-   plugins: [
-     require("@tailwindcss/typography"),
-     require("@tailwindcss/forms"),
-     require("@tailwindcss/aspect-ratio"),
-     require("@tailwindcss/container-queries"),
-   ],
- };
- 
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
+    /* Vil du overskrive eller extende tailwinds indstillinger?  */
+
+	theme: {
+            screens: {
+              'sm': '640px',
+              // => @media (min-width: 640px) { ... }
+        
+              'md': '768px',
+              // => @media (min-width: 768px) { ... }
+        
+              'lg': '924px',
+              // => @media (min-width: 1024px) { ... }
+        
+             
+            },
+
+
+        /* Tilføj din egen farvepalette nedenfor */
+colors: {
+   transparent: 'transparent',
+ current: 'currentColor',
+  'brun': {
+                50: '#F0E5E5',     
+                100: '#DFC8C8',
+                200: '#C29494',
+                300: '#A35D5D',
+                400: '#6F3F3F',
+                500: '#382020',
+                600: '#2D1A1A',
+                700: '#201313',
+                800: '#180D0D',
+                900: '#0A0606',
+                950:'#060404',
+},
+'orange': {
+    50: '#FFEEE5',     
+    100: '#FFDDCC',
+    200: '#FFBB99',
+    300: '#FF9966',
+    400: '#FF7734',
+    500: '#FF5700',
+    600: '#CC4400',
+    700: '#993301',
+    800: '#662300',
+    900: '#331100',
+    950:'#190900',
+},
+
+'blaa': {
+    50: '#FAFCFF',     
+    100: '#F5FAFE',
+    200: '#EBF4FF',
+    300: '#E4F2FF',
+    400: '#DBECFF',
+    500: '#D2E8FF',
+    600: '#75B8FF',
+    700: '#1B88FF',
+    800: '#0059B8',
+    900: '#002C5C',
+    950:'#01152E',
+},
+
+'gron': {
+    50: '#FDFDFB',     
+    100: '#FDFDFB',
+    200: '#FCFCF8',
+    300: '#F9F9F1',
+    400: '#F7F7ED',
+    500: '#F6F6EA',
+    600: '#D9D9A6',
+    700: '#BCBC62',
+    800: '#88883B',
+    900: '#44441C',
+    950:'#20200E',
+},
+
+
+        },
+
+         /* Tilføj din egen spacing nedenfor */
+spacing: {
+    'xxs': '8px',
+    'xs': '16px',
+    's': '24px',
+    'sm': '32px',
+    'm': '40px',
+    'ml': '48px',
+    'lg': '64px',
+    'xl': '80px',
+    '2xl': '96px',
+    '3xl': '112px',
+    '4xl': '128px',
+    '5xl': '112px',
+    '6xl': '128px',
+    '7xl': '112px',
+    '8xl': '128px',
+    '9xl': '424px',
+},
+
+        /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
+fontFamily: {
+    inter: ['Inter', 'sans-serif'],
+    acme: ['Acme', 'sans-serif'],
+},
+
+        /* Tilføj dit typografiske hierarki herunder */
+fontSize: {
+p_desk: ['20px', { lineHeight: '1.25rem' }],
+h4_desk: ['24px', { lineHeight: '2.25rem' }],
+h3_desk: ['31px', { lineHeight: '3rem' }],
+h2_desk: ['36px', { lineHeight: '3.75rem' }],
+h1_desk: ['64px', { lineHeight: '4.5rem' }],
+
+p_mobil: ['12px', { lineHeight: '1.25rem' }],
+h4_mobil: ['14px', { lineHeight: '2.25rem' }],
+h3_mobil: ['17px', { lineHeight: '3rem' }],
+h2_mobil: ['18px', { lineHeight: '' }],
+h1_mobil: ['32px', { lineHeight: '4.5rem' }],
+ },
+
+        /* Tilføj dit fontvægt-hierarki nedenfor */
+fontWeight: {
+    regular: '400',
+    semibold: '600',
+    bold: '800', 
+},
+
+        /* Borders - borderstørrelser herunder */
+        // borderWidth: {
+        //     DEFAULT: '1px',
+        //     0: '0px',
+        //     2: '2px',
+        // },
+
+        /* Border radius størrelser herunder */
+borderRadius: {
+ none: '0px',
+ small: '10px', 
+ medium: '15px',
+ large: '20px',
+ },
+
+        /* Box shadows herunder */
+        // boxShadow: {
+        //     drop25: '4px 4px 4px 0 rgb(0 0 0 / 0.25)', /* x y blur spread farve / opacity */  
+        // },
+	},
+	plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/container-queries'),
+    ],
+}
